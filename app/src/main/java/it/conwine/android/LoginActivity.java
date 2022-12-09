5068eaeccc6fa06e4d5ff3cb2e573fe1ecab59ff
DaveMLBB <davideallega@gmail.com>
2022-12-09 21:56:01 +0100

    first commit

--- app/src/main/java/it/conwine/android/LoginActivity.java
+++ app/src/main/java/it/conwine/android/LoginActivity.java
@@ -2,13 +2,51 @@ package it.conwine.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    EditText username, password;
+    Button login;
+    TextView register, resetPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initializeComponents();
+    }
+
+    private void initializeComponents() {
+        username=findViewById(R.id.editText_username);
+        password=findViewById(R.id.editText_password);
+        login = findViewById(R.id.button_loginuser);
+        register= findViewById(R.id.textView_Register);
+        resetPassword = findViewById(R.id.textView_passwordReset);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.button_loginuser:
+                startActivity(new Intent(LoginActivity.this,
+                        HomeActivity.class ));
+                break;
+            case R.id.textView_Register:
+                switchOnRegister();
+        }
+    }
+
+    private void switchOnRegister() {
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/it/conwine/android/MainActivity.java
+++ app/src/main/java/it/conwine/android/MainActivity.java
@@ -2,13 +2,66 @@ package it.conwine.android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView login;
+    TextView username, email, firstName, lastName, password;
+    Button register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializateComponents();
+        getSupportActionBar();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+    }
+
+    private void initializateComponents() {
+        username = findViewById(R.id.editText_username);
+        email = findViewById(R.id.editText_email);
+        firstName = findViewById(R.id.editText_firstName);
+        lastName = findViewById(R.id.editText_lastName);
+        password = findViewById(R.id.editText_password);
+        login = findViewById(R.id.textView_login);
+
+
+
+    }
+
+
+    @Override
+    public void onClick(View view) {
+
+        switch (view.getId()){
+            case R.id.button_register:
+                Toast.makeText(this, "registered", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.textView_login:
+                switchOnLogin();
+                break;
+            default:
+                
+                break;        
+                
+        }
+
+    }
+
+    private void switchOnLogin() {
+
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
